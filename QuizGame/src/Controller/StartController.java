2e9b21255f07ad39d93b90227dcd80bc35ba9ce9
lobuz <lobuz@zmc>
2018-08-14 22:04:22 +0200

    first commint

---
+++ QuizGame/src/Controller/StartController.java
@@ -0,0 +1,46 @@
+package Controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import Model.Main;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class StartController extends Main implements Initializable{
+
+	@FXML
+	Button next;
+	
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		next.setOnAction(new EventHandler<ActionEvent>() {
+ 
+            @Override
+            public void handle(ActionEvent event) {
+              System.out.println("nastepna scena");
+           
+              	try {   
+              		Parent root2 = FXMLLoader.load(getClass().getResource("/View/GameView.fxml"));
+              		Scene scene2 = new Scene(root2);
+              		Stage gameStage = (Stage)((Node)event.getSource()).getScene().getWindow();
+              		gameStage.setScene(scene2);
+              		gameStage.show();
+              		}
+              	catch(Exception e) {
+              		e.printStackTrace();
+              		}
+              }
+		});
+
+}
+}
---
+++ QuizGame/src/Model/Main.java
@@ -0,0 +1,34 @@
+package Model;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	
+	
+	@Override
+	public void start(Stage startStage) {
+
+		
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("/View/StartView.fxml"));
+			Scene scene = new Scene(root);		
+	        startStage.setScene(scene);
+	        startStage.show();
+			}
+		catch(Exception e) {
+			e.printStackTrace();
+			}
+	}
+	
+}
